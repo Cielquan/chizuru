4c5954ca3237876d375eec596dd7e2279ebe261e
Christian Riedel <cielquan@protonmail.com>
2021-01-21 12:44:26 +0100

    fix problem with missing tests

--- tests/test_chizuru.py
+++ tests/test_chizuru.py
@@ -8,3 +8,13 @@
     :copyright: (c) 2021, Christian Riedel and AUTHORS
     :license: GPL-3.0, see LICENSE for details
 """  # noqa: D205,D208,D400
+from chizuru import chizuru
+
+
+def test_dummy() -> None:
+    """Test a dummy to silence pytest exit code 5."""
+    print(chizuru)
+
+    result = 1 + 1
+
+    assert result == 2
